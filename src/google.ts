915f685f324f3221cd584d08b1009233500f5b69
Jordan Hansen <jbhansen84@gmail.com>
2020-04-08 05:17:41 -0600

    remove googleapis dependency

--- src/google.ts
+++ src/google.ts
@@ -1,4 +1,3 @@
-import { google, books_v1 } from 'googleapis';
 import axios from 'axios';
 import * as dotenv from 'dotenv';
 
@@ -6,20 +5,6 @@ dotenv.config({ path: __dirname + '/../.env' });
 
 
 (async () => {
-
-	// const books = google.books({
-	// 	version: "v1",
-	// 	auth: 'AIzaSyBN0VRQNZBunaT8MLyjQdArM8Tqh4fqFOI'
-	// });
-
-	// const params: books_v1.Params$Resource$Volumes$Get = {
-
-	// };
-
-	// const volumes = await books.volumes.get({
-	// 	auth: 'AIzaSyBN0VRQNZBunaT8MLyjQdArM8Tqh4fqFOI',
-
-	// });
 
 	const axiosResponse = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=${process.env.googleBooksApiKey}&projection=full`);
 
